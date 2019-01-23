9edb2574241bd3302fbf74795ea2b1e207bcb037
DavidHamelin <hamelindavid@hotmail.fr>
2019-01-23 16:55:49 +0100

    correction

--- SQLQuery02.sql
+++ SQLQuery02.sql
@@ -1,5 +1,6 @@
 --SQL - Partie 2 : Création de tables
-/*CREATE DATABASE [webDevelopment];
+/*
+CREATE DATABASE [webDevelopment];
 ATTENTION : selectionner dans explorateur d'objets WebDevelopment/ Tables
 puis faire Nouvelle requete
 
@@ -13,7 +14,9 @@ Table entre crochets avec dbo . avant
 Ex : 
 CREATE TABLE [dbo].[language]
 ***fin bonne pratique***
+*/
 
+/*
 --Ex 01 :
 -- On utilise la DB webDevelopment
 USE webDevelopment
@@ -28,7 +31,7 @@ CREATE TABLE languages (
 	id int IDENTITY(1,1),
 	language nvarchar(255)
 	CONSTRAINT languages_pk PRIMARY KEY (id) 
-);*/
+);
 --The empty "languages" table can now be filled with data with the SQL INSERT INTO statement.
 -- NVARCHAR autorise les accents et limité à 4000 caracteres =! Varchar 255 max
 
@@ -44,27 +47,27 @@ CREATE TABLE frameworks (
 	name nvarchar(255)
 );
 
-ex 04 : 
+--ex 04 : 
 CREATE TABLE libraries (
 	id int IDENTITY(1,1) PRIMARY KEY,
 	library nvarchar(255)
 );
 
-ex 07 : 
+--ex 07 : 
 USE [webDevelopment]
 GO
 DROP TABLE [dbo].[tools];
 
-ex 08 :
+--ex 08 :
 USE [webDevelopment]
 GO
 DROP TABLE [dbo].[libraries];
 
-Ex 09 : 
+--Ex 09 : 
 clic droit, supprimer
+*/
 
-
-TP : 
+/*TP : 
 Base de données > clic droit Créer base de donnée codex
 *codex
 **Tables > créer table
